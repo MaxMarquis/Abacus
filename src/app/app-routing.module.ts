dd448b5bf54aa71a90ca3d000d21af1769640cdf
Josée Gervais <j_gervais07@hotmail.com>
2022-05-03 22:02:40 -0400

    Voici la nouvelle calculatrice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './components/first/first.component';
+import { CalculatriceComponent } from './components/calculatrice/calculatrice.component';
+
 
 const routes: Routes = [
   { path: '', component: FirstComponent },
+  { path: 'calculatrice', component: CalculatriceComponent },
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { FirstComponent } from './components/first/first.component';
+import { CalculatriceComponent } from './components/calculatrice/calculatrice.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FirstComponent
+    FirstComponent,
+    CalculatriceComponent
+   
   ],
   imports: [
     BrowserModule,
