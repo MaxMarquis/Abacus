d5f0e233c9247cca6f15824702a898823f07f39b
Josée Gervais <j_gervais07@hotmail.com>
2022-05-08 21:02:03 -0400

    Voici la navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './components/first/first.component';
+import { CalculatriceComponent } from './components/calculatrice/calculatrice.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RapportsComponent } from './components/rapports/rapports.component';
+import { TableauDeBordComponent } from './components/tableau-de-bord/tableau-de-bord.component';
+import { AideComponent } from './components/aide/aide.component';
+import { DeconnexionComponent } from './components/deconnexion/deconnexion.component';
+import { CalendrierComponent } from './components/calendrier/calendrier.component';
+
 
 const routes: Routes = [
   { path: '', component: FirstComponent },
+  { path: 'calculatrice', component: CalculatriceComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'rapports', component: RapportsComponent },
+  { path: 'tableauDeBord', component: TableauDeBordComponent },
+  { path: 'aide', component: AideComponent },
+  { path: 'deconnexion', component: DeconnexionComponent },
+  { path: 'calendrier', component: CalendrierComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { FirstComponent } from './components/first/first.component';
+import { CalculatriceComponent } from './components/calculatrice/calculatrice.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RapportsComponent } from './components/rapports/rapports.component';
+import { TableauDeBordComponent } from './components/tableau-de-bord/tableau-de-bord.component';
+import { AideComponent } from './components/aide/aide.component';
+import { DeconnexionComponent } from './components/deconnexion/deconnexion.component';
+import { CalendrierComponent } from './components/calendrier/calendrier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FirstComponent
+    FirstComponent,
+    CalculatriceComponent,
+    NavbarComponent,
+    RapportsComponent,
+    TableauDeBordComponent,
+    AideComponent,
+    DeconnexionComponent,
+    CalendrierComponent
   ],
   imports: [
     BrowserModule,
