fec4920f3eedce5876ab4e4e70830f59ace0a027
Danny Beaunoyer <74875186+dbeaunoyer@users.noreply.github.com>
2022-05-10 15:28:21 -0400

    ajout_depenses-revenus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { StorageServiceService } from './services/storage-service.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { FirstComponent } from './components/first/first.component';
+import { CalculatriceComponent } from './components/calculatrice/calculatrice.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RapportsComponent } from './components/rapports/rapports.component';
+import { TableauDeBordComponent } from './components/tableau-de-bord/tableau-de-bord.component';
+import { AideComponent } from './components/aide/aide.component';
+import { DeconnexionComponent } from './components/deconnexion/deconnexion.component';
+import { CalendrierComponent } from './components/calendrier/calendrier.component';
+import { ExpenseComponentComponent } from './expense-component/expense-component.component';
+import { IncomeComponentComponent } from './income-component/income-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FirstComponent,
+    CalculatriceComponent,
+    NavbarComponent,
+    RapportsComponent,
+    TableauDeBordComponent,
+    AideComponent,
+    DeconnexionComponent,
+    CalendrierComponent,
+    ExpenseComponentComponent,
+    IncomeComponentComponent,
+
 
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [StorageServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/expense-component/expense-component.component.ts
@@ -0,0 +1,60 @@
+import { v4 as uuid } from 'uuid';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { Details } from '../interface/details';
+import { StorageServiceService } from '../services/storage-service.service';
+
+
+@Component({
+  selector: 'app-expense-component',
+  templateUrl: './expense-component.component.html',
+  styleUrls: ['./expense-component.component.sass']
+})
+export class ExpenseComponentComponent {
+  @ViewChild('fform') feedbackFormDirective: any;
+  submitForm!: FormGroup;
+  details!: Details;
+  isValidMontantError: string = "";
+  storageIncomeKey: string = environment.storageIncomeKey;
+  storageExpenseKey: string = environment.storageExpenseKey;
+  balance: number = 0;
+  expenseList: Details[] = [];
+
+  constructor(private fb: FormBuilder, private storageService: StorageServiceService) {
+    this.createForm();
+    this.storageService.expenseList.subscribe(value => {
+      this.expenseList = value;
+    }
+    );
+    this.storageService.balanceValue.subscribe(value => {
+      this.balance = value;
+    }
+    );
+  }
+
+  createForm(): void {
+    this.submitForm = this.fb.group({
+      description: ['', Validators.required],
+      montant: [0, [Validators.required, Validators.pattern("^[0-9]+[0-9]*$")]],
+      date: [Date.now, Validators.required]
+    });
+
+  }
+
+
+  onSubmit(): void {
+    this.details = this.submitForm.value;
+    this.details.id = uuid();
+    this.storageService.addExpense(this.details);
+
+    this.submitForm.reset({
+      description: '',
+      montant: 0,
+      date: new Date()
+    });
+
+    this.expenseList.push(this.details);
+    this.feedbackFormDirective.resetForm();
+  }
+}
