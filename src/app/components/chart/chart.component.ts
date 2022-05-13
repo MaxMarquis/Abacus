f23bd7da4359b10e62430c09a647750e8751ad85
Max Laptop <maxim.marquis@gmail.com>
2022-05-13 12:36:22 -0400

    Création du graphique style donuts avec un style dynamique

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.sass']
+  styleUrls: ['./chart.component.sass'],
 })
 export class ChartComponent implements OnInit {
-
-  constructor() { }
+  @Input() height = '25';
+  @Input() width = '25';
 
   ngOnInit(): void {
+    console.log(`Mon height ${this.height} et mon width ${this.width}`);
   }
 
+  // Doughnut
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100] },
+      { data: [50, 150, 120] },
+      { data: [250, 130, 70] },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: {}[];
+  }): void {
+    console.log(event, active);
+  }
 }
