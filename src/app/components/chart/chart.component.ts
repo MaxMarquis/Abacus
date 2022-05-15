b3ab0abb91f69883df24fed68c2b55574bebcefe
Maxim Marquis <59632275+MaxMarquis@users.noreply.github.com>
2022-05-15 13:29:25 -0400

    Maj du composant pour être réutilisable

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,61 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ChartData, ChartEvent, ChartType } from 'chart.js';
+import { ChartData, ChartType } from 'chart.js';
 import { Details } from 'src/app/interface/details';
-import { StorageServiceService } from 'src/app/services/storage-service.service';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.sass'],
 })
+
 export class ChartComponent implements OnInit {
+  // Récupère les props afin de personnaliser le graphique
   @Input() height = '25';
   @Input() width = '25';
+  @Input() dataList: Details[] = []
 
-  public expenseList: Details[] = [];
-  public doughnutChartLabels: string[] = [];
-
-  constructor(private storageService: StorageServiceService) {
-    this.storageService.expenseList.subscribe((res) => {
-      this.expenseList = res;
-    });
-  }
-
-  // this.doughnutChartData.datasets.push(x.montant)
+  constructor() { }
 
   ngOnInit(): void {
-    console.log(this.doughnutChartData.datasets);
-    this.expenseList.map((x) => {
+    // Récupère le data et envoie les descriptions dans doughnutChartLabels et les montants dans doughnutChartData
+    this.dataList.map((x) => {
       this.doughnutChartLabels.push(x.description);
       this.doughnutChartData.datasets[0].data.push(x.montant);
     });
   }
 
-  // Doughnut
+  // Création du graphique style Doughnut
+  public doughnutChartLabels: String[] = [];
   public doughnutChartData: ChartData<'doughnut'> = {
     labels: this.doughnutChartLabels,
     datasets: [{ data: [] }],
   };
   public doughnutChartType: ChartType = 'doughnut';
-
-  // events
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event: ChartEvent;
-    active: {}[];
-  }): void {
-    console.log(event, active);
-  }
-
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event: ChartEvent;
-    active: {}[];
-  }): void {
-    console.log(event, active);
-  }
 }
