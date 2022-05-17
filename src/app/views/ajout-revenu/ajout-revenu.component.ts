8e5a09f1d32b559d22972309ba5fd06906f5dcec
Maxim Marquis <59632275+MaxMarquis@users.noreply.github.com>
2022-05-16 22:25:20 -0400

    Première partie de l'ajout de revenu

--- src/app/views/ajout-revenu/ajout-revenu.component.ts
+++ src/app/views/ajout-revenu/ajout-revenu.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { environment } from 'src/environments/environment';
+import { Details } from 'src/app/interface/details';
+import { StorageServiceService } from 'src/app/services/storage-service.service';
 
 @Component({
   selector: 'app-ajout-revenu',
   templateUrl: './ajout-revenu.component.html',
   styleUrls: ['./ajout-revenu.component.sass']
 })
-export class AjoutRevenuComponent implements OnInit {
 
-  constructor() { }
+export class AjoutRevenuComponent {
+  @ViewChild('fform') feedbackFormDirective: any;
+  submitForm!: FormGroup;
+  details!: Details;
+  isValidMontantError: string = "";
+  storageIncomeKey: string = environment.storageIncomeKey;
+  storageExpenseKey: string = environment.storageExpenseKey;
+  balance: number = 0;
+  incomeList: Details[] = [];
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private storageService: StorageServiceService, private modalService: NgbModal) {
+    this.createForm();
+    this.storageService.incomeList.subscribe(value => {
+      this.incomeList = value;
+    });
+    this.storageService.balanceValue.subscribe(value => {
+      this.balance = value;
+    });
+  }
+
+  createForm(): void {
+    this.submitForm = this.fb.group({
+      description: ['', Validators.required],
+      montant: [0, [Validators.required, Validators.pattern("^[0-9-.]+[0-9-.]*$")]],
+      date: [Date.now, Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    this.details = this.submitForm.value;
+    this.details.id = uuid();
+    this.storageService.addIncome(this.details);
+
+    this.submitForm.reset({
+      description: '',
+      montant: 0,
+      date: new Date()
+    });
+
+    this.incomeList.push(this.details);
+    this.feedbackFormDirective.resetForm();
+
+    location.reload(); // Pour reload le graphique
+  }
+
+  openCalculatorModal(content: any) {
+    this.modalService.open(content);
+  }
+  deleteIncome(id: number) {
+    this.incomeList = this.incomeList.filter((v, i) => i !== id);
   }
 
 }
