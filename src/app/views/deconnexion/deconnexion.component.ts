0b8b247a06fdd44b7f540de2e036a0838854c10c
Maxim Marquis <59632275+MaxMarquis@users.noreply.github.com>
2022-05-16 22:24:50 -0400

    Ajout de la page déconnexion

---
+++ src/app/views/deconnexion/deconnexion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deconnexion',
+  templateUrl: './deconnexion.component.html',
+  styleUrls: ['./deconnexion.component.sass']
+})
+export class DeconnexionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
