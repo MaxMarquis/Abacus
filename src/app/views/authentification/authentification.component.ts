1a52d4225a4cb1d314da17d498adb62098babd7e
Whistler Clermont <clermontwhistler2@gmail.com>
2022-05-23 12:26:21 -0400

    ajout de modification pour l'authentification

--- src/app/views/authentification/authentification.component.ts
+++ src/app/views/authentification/authentification.component.ts
@@ -12,12 +12,16 @@ export class AuthentificationComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  valider(firstname: string, email: string) {
-    if (firstname === '') {
-      alert('le champs Nom utilisateur ne doit pas etre vide ');
+  valider(password: string, email: string) {
+    if ((password === '')&&(email === "")){
+      alert('Il faut entrer votre Email et votre mot de passe pour vous connecter');
     } else if (email === '') {
       alert('le champs Email ne doit etre vide');
-    } else {
+    } 
+    else if (password === '') {
+      alert('Vous devez entrer le mot de passe ou alors le mot de passe est incorrect ');
+    }
+    else {
       this.router.navigateByUrl('/tableauDeBord')
     }
   }
