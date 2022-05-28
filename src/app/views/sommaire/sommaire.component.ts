cd0d5ae0e7c4d89297ce94ce7900798f357a0d6f
Maxim Marquis <59632275+MaxMarquis@users.noreply.github.com>
2022-05-28 12:20:40 -0400

    Indentation

--- src/app/views/sommaire/sommaire.component.ts
+++ src/app/views/sommaire/sommaire.component.ts
@@ -11,48 +11,36 @@ import { StorageServiceService } from 'src/app/services/storage-service.service'
 export class SommaireComponent implements OnInit {
 
   incomeExpenseList: Details[] = [];
-  SummaryIncome : Details[] = [];
-  SummaryDepense : Details[] = [];
+  SummaryIncome: Details[] = [];
+  SummaryDepense: Details[] = [];
 
   constructor(private canonicApiService: CanonicApiService) {
-    
   }
 
-//   * affichage des données revenu/dépenses dans le sommaire des transactions
-
+  //   * affichage des données revenu/dépenses dans le sommaire des transactions
 
   ngOnInit() {
     // * celle ci est la table détails.
     this.canonicApiService.getIncomeExpenseList().subscribe(
       (response: any) => {
         console.log(response);
-        this.incomeExpenseList = response.data; 
-        
-      },
-      () => console.log('error')
+        this.incomeExpenseList = response.data;
+      }, () => console.log('error')
     );
 
-  // * cette fonction permet d'ajouter les revenus via la table des revenus dans le sommaire des transactions
+    // * cette fonction permet d'ajouter les revenus via la table des revenus dans le sommaire des transactions
     this.canonicApiService.getSummaryIncome().subscribe(
       (response: any) => {
         console.log(response);
-        this.SummaryIncome = response.data; 
-      },
-      () => console.log('error')
+        this.SummaryIncome = response.data;
+      }, () => console.log('error')
     );
-      // * cette fonction permet d'ajouter les dépenses via la table des dépenses dans le sommaire des transactions
-     this.canonicApiService.getSummaryDepense().subscribe(
+    // * cette fonction permet d'ajouter les dépenses via la table des dépenses dans le sommaire des transactions
+    this.canonicApiService.getSummaryDepense().subscribe(
       (response: any) => {
         console.log(response);
-        this.SummaryDepense = response.data; 
-      },
-      () => console.log('error')
+        this.SummaryDepense = response.data;
+      }, () => console.log('error')
     );
-    
   }
-
-
-  
-  
-
 }
